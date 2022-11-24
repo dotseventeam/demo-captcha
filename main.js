8800674eac123d893ac7807e5703c1e0695ac523
ivanarena <ivan.x28x@gmail.com>
2022-11-24 09:56:13 +0100

    funzioni di base js

---
+++ main.js
@@ -0,0 +1,28 @@
+let modal = document.querySelector('.captcha-modal');
+let login = document.getElementById('login-btn');
+let submit = document.getElementById('captcha-submit');
+let successPage = 'http://127.0.0.1:5500/logged.html';
+
+function showCaptcha() {
+    modal.classList.add('is-active');
+};
+
+function loginSuccess() {
+    window.location.href = successPage;
+}
+
+function loginFailed() {
+    console.log('login failed');
+}
+
+function validateCaptcha() {
+    let answers = [true, true]; // replace with real answers
+    if (answers[0] && answers[1]) { // change conditions
+        loginSuccess();
+    } else {
+        loginFailed();
+    }
+};
+
+login.addEventListener('click', showCaptcha);
+submit.addEventListener('click', validateCaptcha);
